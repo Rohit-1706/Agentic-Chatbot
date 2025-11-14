93644bb0731ad7030b42f7a3e9ea7acb3052b067
rohit-1706 <chowdhurysubham125@gmail.com>
2025-11-14 08:35:55 +0530

    Basic chatbot

---
+++ src/langgraphagenticai/ui/streamlitui/loadui.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import os
+
+from src.langgraphagenticai.ui.uiconfigfile import Config
+
+class LoadStreamlitUI:
+    def __init__(self):
+        self.config = Config()
+        self.user_controls = {}
+
+    def load_streamlit_ui(self):
+        st.set_page_config(page_title="🤖" + self.config.get_page_title(), layout="wide")
+        st.header(" 🤖 " + self.config.get_page_title())
+
+
+        with st.sidebar:
+            # Get options from config
+            llm_options = self.config.get_llm_options()
+            usecase_options = self.config.get_usecase_options()
+
+            # LLM Selection
+            self.user_controls["selected_llm"] = st.selectbox("Select LLM", llm_options)
+
+            if self.user_controls["selected_llm"] == "OpenAI":
+                # Model Selection
+                model_options = self.config.get_openai_model_options()
+                self.user_controls["selected_openai_model"] = st.selectbox("Select Model", model_options)
+                self.user_controls["OPENAI_API_KEY"] = st.session_state["OPENAI_API_KEY"] = st.text_input("API Key", type="password")
+                
+                # Validate API Key
+                if not self.user_controls["OPENAI_API_KEY"]:
+                    st.warning("⚠️ Please enter your OPENAI API Key to proceed.")
+
+            # Usecase selection
+            self.user_controls["selected_usecase"] = st.selectbox("Select Usecases", usecase_options)
+
+
+        
+            return self.user_controls
